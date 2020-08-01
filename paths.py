2c05cae57ec5d199ab92f9cd37f168a311858df8
Cengiz Batur <cengizb1925@gmail.com>
2020-08-01 23:04:23 +0300

    first commit

---
+++ paths.py
@@ -0,0 +1,78 @@
+username_field = '//*[@id="username"]'
+pass_field = '//*[@id="password"]'
+username = 'kerim-imaj'
+pw = 'Kerim1234!'
+master_islem_tipi = '/html/body/div[4]/div/form[1]/fieldset/div/div[1]/table/tbody/tr[1]/td[2]/div/div[3]/span'
+cekim_bt = '//*[@id="j_idt335_2"]'
+islem_tipi = '/html/body/div[4]/div/form[1]/fieldset/div/div[1]/table/tbody/tr[1]/td[8]/div/div[3]/span'
+search_box = '//*[@id="transTypes_panel"]/div[1]/div[2]/input'
+btc_sec = '//*[@id="transTypes_panel"]/div[2]/ul/li[4]/div/div[2]/span'
+tum_kutucuklar = '//*[@id="transTypes_panel"]/div[1]/div[1]/div[2]/span'
+bosluk = '//*[@id="customerDepositWithdrawSearchPanel"]'
+search_bt = '//*[@id="searchButton"]/span'
+musteri_kodu = '/html/body/div[4]/div/form[1]/fieldset/div/div[3]/div/div[1]/div[1]/div/div[2]/table/tbody/tr/td[14]'
+customer_search = '/html/body/div[4]/div/div[5]/div/form[1]/fieldset/div/table/tbody/tr[1]/td[1]/input'
+arama_bt = '/html/body/div[4]/div/div[5]/div/form[1]/fieldset/div/table/tbody/tr[2]/td/button/span'
+islemler_bt = '//*[@id="form:tabView"]/ul/li[9]/a'
+baslangic_tarihi = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[1]/table/tbody/tr[1]/td[2]/span/input'
+ara = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[1]/table/tbody/tr[2]/td[9]/button/span'
+islem = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[2]/table/tbody/tr/td[6]'
+sayfa_nr = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[3]/select'           
+sayfa_50 = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[3]/select/option[4]'
+durum_tipi = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[1]/table/tbody/tr[2]/td[6]/div/label'
+durum_tamamlandi = '/html/body/div[27]/div/ul/li[5]'
+islem_type = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[1]/table/tbody/tr[1]/td[6]/div/span/label'
+hepsini_sec = '/html/body/div[24]/div[1]/div[1]/div[2]/span'
+search_box2 = '/html/body/div[24]/div[1]/div[2]/input'
+win_bet_box = '/html/body/div[24]/div[2]/ul/li[58]/div/div[2]/span'
+win_bet_box2 = '/html/body/div[24]/div[2]/ul/li[59]/div/div[2]/span'
+bet = '/html/body/div[2]/div[2]/form/div/div[3]/div/div[2]/table/tbody/tr/td[10]'
+text_bosluk = '/html/body/div[3]/textarea'
+istatistikler_bt = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/ul/li[2]/a'
+para_cekim = '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[2]/div/div/div[2]/div[1]/div[2]/table/tbody/tr[3]/td[3]/label'
+tutar = '/html/body/div[4]/div/form[1]/fieldset/div/div[3]/div/div[1]/div[1]/div/div[2]/table/tbody/tr/td[7]'
+gecerli_bt = '/html/body/div[4]/div/form[1]/fieldset/div/div[2]/div[1]/button[2]/span'
+
+#---CASINO PANELI-----#
+kod_path = '/html/body/div[1]/form[2]/div/div[2]/table/tbody/tr[1]/td[3]/input'
+giris_kodu = '7070'
+username_path = '/html/body/div[1]/form[2]/div/div[2]/table/tbody/tr[2]/td[3]/input'
+giris_username = 'Emre1'
+password_path = '/html/body/div[1]/form[2]/div/div[2]/table/tbody/tr[3]/td[3]/input'
+giris_password = 'Emre1234*'
+casino_kod = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[1]/div/div[2]/table/tbody/tr[2]/td[3]/input'
+casino_ara = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[1]/div/div[2]/table/tbody/tr[3]/td/button/span'
+casino_musteri = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[2]/div[2]/div[2]/table/tbody/tr'
+casino_degistir = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[2]/div[1]/div/button'
+casino_hh = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[2]/div/div/ul/li[3]/a'
+casino_tarih = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[2]/div/div/div/div[3]/table/tbody/tr[1]/td[3]/span/input'
+casino_bahis_ara = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[2]/div/div/div/div[3]/table/tbody/tr[2]/td[5]/button/span'
+casino_har_tip = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[2]/div/div/div/div[3]/table/tbody/tr[2]/td[3]/div/label'
+casino_playbet = '/html/body/div[9]/div/ul/li[2]'
+casino_bosluk = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[2]/div/div/div/div[3]'
+casino_toplam_bahis = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[2]/div/div/div/div[3]/div/div[4]/div/table/tfoot/tr/td[4]'
+casino_kapat = '/html/body/table/tbody/tr[2]/td/fieldset/div/form[4]/div/div[1]/a[1]/span'
+#---CASINO PANELI-----#
+
+tum_yatirim_yontemleri = [
+    'Anında BTC Yatırım', 
+    'Anında Havale Deposit',
+    'Anında Kredi Kartı Deposit',
+    'Anında Papara Deposit',
+    'Blokesiz Dekontsuz Havale Deposit',
+    'Blokesiz Hızlı Papara Deposit',
+    'Blokesiz Kredi Kartı Deposit',
+    'Blokesiz Kredi Kartına Havale Deposit',
+    'Blokesiz Manual Papara Deposit',
+    'Deposit with AstropayCard to Sport Balance',
+    'Deposit with Ecopayz to Sport Balance',
+    'Envoy Hızlı QR Yatırım',
+    'Hızlı Qr İle Para Yatır',
+    'Jet Papara Deposit',
+    'Maksipara Papara Deposit',
+    'Ödendibil Cepbank Deposit',
+    'Payfix Deposit',
+    'Payturka CMT Deposit',
+    'Payznet Kredi Kartı ile Yatırım',
+    'Superhavale Deposit',
+    ]
---
+++ pronet/vevo_cek.py
@@ -0,0 +1,406 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException, ElementNotInteractableException
+import time
+import sys
+sys.path.insert(0, '/Users/serkankayser/Desktop/cekimkt/')
+from paths import username_field, pass_field, username, pw, master_islem_tipi, cekim_bt, islem_tipi
+from paths import search_box, btc_sec, tum_kutucuklar, bosluk, search_bt, musteri_kodu, customer_search, arama_bt
+from paths import islemler_bt, baslangic_tarihi, ara, islem, sayfa_nr, sayfa_50, tum_yatirim_yontemleri, durum_tipi, durum_tamamlandi
+from paths import islem_type, hepsini_sec, search_box2, win_bet_box, win_bet_box2, bet, text_bosluk
+from paths import password_path, kod_path, username_path, giris_kodu, giris_username, giris_password
+from paths import casino_kod, casino_ara, casino_musteri, casino_degistir, casino_hh, casino_tarih, casino_bahis_ara, casino_har_tip, casino_playbet, casino_bosluk, casino_toplam_bahis, casino_kapat
+from paths import istatistikler_bt, para_cekim, tutar
+
+# driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[0]) # LOGIN PANELI
+# driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[1]) # CASINO PANELI
+# driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[2]) # Muhasebe Yönetimi
+# driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[3]) # DONTPAD
+# driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[4]) # Yeni Müşteri Ara Paneli
+
+all_customer_ids = []
+islem_sutunu = []
+tarih_sutunu = []
+cr_page_number = 2
+tum_cek_miktarlari = []
+
+class vevo_panel():
+    chrome_option = Options()
+    # chrome_option.add_argument('--headless')
+    chrome_option.add_argument('--no-sandbox')
+    chrome_option.add_argument('--disable-dev-shm-usage')
+    chrome_option.add_argument("--window-size=1920,1080")
+    driver = webdriver.Chrome(chrome_options=chrome_option)
+    driver.get("https://dagur.pronetgaming.eu/login.xhtml")
+
+driver_vevo = vevo_panel()
+
+def check_exists_by_xpath(xpath):
+    try:
+        wait = WebDriverWait(driver_vevo.driver, 20)
+        wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+    finally:
+        return False
+    return True
+
+def login():
+    username_bt = driver_vevo.driver.find_element_by_xpath(username_field)
+    username_bt.click()
+    username_bt.send_keys(username)
+    pass_bt = driver_vevo.driver.find_element_by_xpath(pass_field)
+    pass_bt.click()
+    pass_bt.send_keys(pw)
+    pass_bt.send_keys(Keys.ENTER)
+    time.sleep(10)
+    driver_vevo.driver.execute_script("window.open('https://dagur.pronetgaming.eu/restricted/cust-money-dep-withdraw.xhtml')")
+    time.sleep(3)
+    login_casino()
+
+def login_casino():
+    driver_vevo.driver.execute_script("window.open('https://casinotrader-sl.pronetgaming.eu/login.xhtml')")  
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[1]) # CASINO PANELI
+    kod = driver_vevo.driver.find_element_by_xpath(kod_path)
+    kod.click()
+    kod.send_keys(giris_kodu)    
+    user = driver_vevo.driver.find_element_by_xpath(username_path)
+    user.click()
+    user.send_keys(giris_username)
+    pass_bt = driver_vevo.driver.find_element_by_xpath(password_path)
+    pass_bt.click()
+    pass_bt.send_keys(giris_password)
+    pass_bt.send_keys(Keys.ENTER) 
+    time.sleep(10)
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[2]) # Muhasebe Yönetimi
+    get_ready_panel()
+
+def get_ready_panel():
+    check_exists_by_xpath(master_islem_tipi)
+    driver_vevo.driver.find_element_by_xpath(master_islem_tipi).click()
+    driver_vevo.driver.find_element_by_xpath(cekim_bt).click()
+    time.sleep(1)
+    driver_vevo.driver.find_element_by_xpath(islem_tipi).click()
+    time.sleep(1)
+    driver_vevo.driver.find_element_by_xpath(tum_kutucuklar).click() # TUM KUTUCUKLARI SEC - ISLEM TIPINDE
+    # BTC CEKIMI CIKAR - BASLANGIC
+    search_b = driver_vevo.driver.find_element_by_xpath(search_box)
+    search_b.click()
+    time.sleep(1)
+    search_b.send_keys('btc')
+    driver_vevo.driver.find_element_by_xpath(btc_sec).click()
+    time.sleep(1) # SIL
+    # BTC CEKIMI CIKAR - BITIS
+    driver_vevo.driver.find_element_by_xpath(bosluk).click()
+    driver_vevo.driver.find_element_by_xpath(search_bt).click()
+    driver_vevo.driver.execute_script("window.open('https://dagur.pronetgaming.eu/restricted/customer-details.xhtml?faces-redirect=true&customerId=6090455')") 
+    time.sleep(3)
+    driver_vevo.driver.execute_script("window.open('http://dontpad.com/kerim.cek.kt')")
+    get_cust_id()
+
+def get_id_again():
+    driver_vevo.driver.find_element_by_xpath(search_bt).click()
+    get_cust_id()
+
+def get_cust_id():
+    time.sleep(3)
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[2]) # Muhasebe Yönetimi
+    a = driver_vevo.driver.find_elements_by_xpath(musteri_kodu)
+    for customer_id in a:
+        all_customer_ids.append(customer_id.text)
+    
+    b = driver_vevo.driver.find_elements_by_xpath(tutar)
+    for tutarlar in b:
+        yeni_tutar = tutarlar.text.split(',')
+        filtred_tutar = int(yeni_tutar[0].replace('.', ''))
+        tum_cek_miktarlari.append(filtred_tutar)
+    if len(all_customer_ids) == 0:
+        time.sleep(10) # YENI CEKIM GELMESINI BEKLE 10 SANIYE
+        get_id_again()
+    cp_paste_cust_id()
+
+def cp_paste_cust_id():
+    if tum_cek_miktarlari[-1] >= 2000: # KAC TLYE KADAR KT EDILMESINI ISTIYORSAN BURDAN AYARLA (1999 TL ve altindaki miktarlar kt ediliyor)
+        del tum_cek_miktarlari[-1]
+        del all_customer_ids[-1]
+    if len(all_customer_ids) == 0:
+        time.sleep(10) # YENI CEKIM GELMESINI BEKLE 10 SANIYE
+        get_id_again()
+    print(all_customer_ids, tum_cek_miktarlari)    
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[4]) # Yeni Müşteri Ara Paneli
+    time.sleep(1)
+    customer_search_box = driver_vevo.driver.find_element_by_xpath(customer_search)
+    customer_search_box.click()
+    customer_search_box.send_keys(all_customer_ids[-1]) # DEGISTIR!!!!
+    driver_vevo.driver.find_element_by_xpath(arama_bt).click()
+    time.sleep(5)
+    get_ready_islemler()
+
+def get_ready_islemler():
+    driver_vevo.driver.find_element_by_xpath(islemler_bt).click() # ISLEMLER BUTONUNA TIKLA
+    # TARIHI 1 AY GERIYE AL - BASLANGIC
+    check_exists_by_xpath(baslangic_tarihi)
+    bt = driver_vevo.driver.find_element_by_xpath(baslangic_tarihi)
+    tarih = bt.get_attribute('value') # TARIH DEGERINI AL
+    gun = tarih[0:2]
+    ay = tarih[3:5]
+    sene = tarih[6:10]
+    yeni_ay = int(ay) - 1
+    bt.clear()
+    bt.send_keys(f'{gun}.{yeni_ay}.{sene} 23:59:59') 
+    # TARIHI 1 AY GERIYE AL - BITIS
+
+    # DURUM TIPINI TAMAMLANMIS YAP - BASLANGIC
+    driver_vevo.driver.find_element_by_xpath(durum_tipi).click()
+    driver_vevo.driver.find_element_by_xpath(durum_tamamlandi).click()    
+    # DURUM TIPINI TAMAMLANMIS YAP - BITIS
+
+    # WIN_BETI SECME - BASLANGIC
+    driver_vevo.driver.find_element_by_xpath(islem_type).click()
+    driver_vevo.driver.find_element_by_xpath(hepsini_sec).click()
+    win_yazdir = driver_vevo.driver.find_element_by_xpath(search_box2)
+    win_yazdir.click()
+    win_yazdir.send_keys('win')
+    driver_vevo.driver.find_element_by_xpath(win_bet_box).click()
+    driver_vevo.driver.find_element_by_xpath(win_bet_box2).click()
+    time.sleep(1)
+    # WIN_BETI SECME - BITIS
+  
+    driver_vevo.driver.find_element_by_xpath(ara).click() # ARAMA BUTONUNA TIKLA
+    time.sleep(3)
+
+    # SAYFA SAYISINI 50 YAP - BASLANGIC
+    driver_vevo.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver_vevo.driver.find_element_by_xpath(sayfa_nr).click()
+    time.sleep(1)
+    driver_vevo.driver.find_element_by_xpath(sayfa_50).click()
+    # SAYFA SAYISINI 50 YAP - BITIS
+
+    get_wd_data() # SONRAKI FONKSIYONA GEC
+
+def cekim_onay(note):
+    # MESAJ YAZDIR - BASLANGIC
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[3]) # DONTPAD SAYFASINA GEC
+    bosluga_tikla = driver_vevo.driver.find_element_by_xpath(text_bosluk)
+    bosluga_tikla.click()
+    bosluga_tikla.send_keys(note)
+    bosluga_tikla.send_keys(Keys.RETURN)
+    # MESAJ YAZDIR - BITIS
+    time.sleep(2)
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[2]) # Muhasebe Yönetimi
+
+    for cek in driver_vevo.driver.find_elements_by_xpath(musteri_kodu):
+        if cek.text == all_customer_ids[-1]:
+            cek.click()
+            break
+    time.sleep(5)
+
+    # driver_vevo.driver.find_element_by_xpath(gecerli_bt).click # GECERLIYE ATMAK ICIN # YI KALDIR.
+            
+    del all_customer_ids[-1] # LISTEDEKI SON MUSTERININ IDSINI SIL
+    del tum_cek_miktarlari[-1] # LISTEDEKI SON MUSTERININ CEKIM MIKTARINI SIL
+    islem_sutunu.clear()
+    tarih_sutunu.clear()
+    cr_page_number = 2
+    if len(all_customer_ids) == 0:
+        driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[2])
+        driver_vevo.driver.find_element_by_xpath(search_bt).click()
+        get_cust_id()
+    else:
+        
+        cp_paste_cust_id()
+
+def cekim_red(note):
+    # MESAJ YAZDIR - BASLANGIC
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[3]) # DONTPAD SAYFASINA GEC
+    bosluga_tikla = driver_vevo.driver.find_element_by_xpath(text_bosluk)
+    bosluga_tikla.click()
+    bosluga_tikla.send_keys(note)
+    bosluga_tikla.send_keys(Keys.RETURN)
+    # MESAJ YAZDIR - BITIS
+    del all_customer_ids[-1] # LISTEDEKI SON MUSTERI IDSINI SIL
+    del tum_cek_miktarlari[-1] # LISTEDEKI SON MUSTERININ CEKIM MIKTARINI SIL
+    islem_sutunu.clear()
+    tarih_sutunu.clear()
+    cr_page_number = 2
+    if len(all_customer_ids) == 0:
+        driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[2])
+        driver_vevo.driver.find_element_by_xpath(search_bt).click()
+        get_cust_id()
+    else:
+        cp_paste_cust_id()
+
+def get_wd_data():
+    time.sleep(3)
+    islem_sutunu = driver_vevo.driver.find_elements_by_xpath(islem)
+    
+    a = [] # GECICI LISTE ISLEM ICIN
+    c = [] # GECICI LISTE MIKTAR ICIN
+    for b in islem_sutunu:
+        a.append(b.text)
+        if 'Withdraw' in a[-1]:
+            note = f'{all_customer_ids[-1]} : Son cekiminden once cekimi var. ONAY'
+            cekim_onay(note)
+
+        elif b.text in tum_yatirim_yontemleri:
+            break
+    islem_sutunu = a # ISLEM TIPLERI
+    adet = len(islem_sutunu) + 1
+    for i in range(1,adet):
+        tarih = driver_vevo.driver.find_element_by_xpath(
+            f'/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[2]/table/tbody/tr[{i}]/td[5]')
+        tarih_sutunu.append(tarih.text)        
+        
+        miktar_sutunu = driver_vevo.driver.find_element_by_xpath(
+            f'/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[2]/table/tbody/tr[{i}]/td[7]')
+        miktar_filtre = miktar_sutunu.text[:-3]
+        if len(miktar_filtre) > 4:
+            miktar_filtre = miktar_filtre.replace('.', '')
+        c.append(miktar_filtre)
+    
+    miktar_sutunu = c # MIKTARLAR
+    cevrim_hesapla(miktar_sutunu, islem_sutunu, tarih_sutunu)
+
+def istatistik():
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[4]) # Yeni Müşteri Ara Paneli
+    driver_vevo.driver.execute_script("window.scrollTo(0, 0)")
+    driver_vevo.driver.find_element_by_xpath(istatistikler_bt).click()
+    time.sleep(4)
+    if not driver_vevo.driver.find_element_by_xpath(para_cekim):
+        istatistik()
+    para_cekme_miktari = driver_vevo.driver.find_element_by_xpath(para_cekim).text
+
+    print(f'Para cekme miktari : {para_cekme_miktari}')
+    if para_cekme_miktari == '0,00':
+        return para_cekme_miktari
+
+def casino_hesapla(deposit_miktari):
+    driver_vevo.driver.switch_to_window(driver_vevo.driver.window_handles[1]) # CASINO PANELI
+    
+    kod_yapistir = driver_vevo.driver.find_element_by_xpath(casino_kod)
+    kod_yapistir.click()
+    kod_yapistir.send_keys(all_customer_ids[-1])
+
+    driver_vevo.driver.find_element_by_xpath(casino_ara).click()
+    time.sleep(5)
+    driver_vevo.driver.find_element_by_xpath(casino_musteri).click()
+    time.sleep(2)
+    driver_vevo.driver.find_element_by_xpath(casino_degistir).click()
+    time.sleep(2)
+    driver_vevo.driver.find_element_by_xpath(casino_hh).click()
+    time.sleep(2)
+    
+    cas_tarih = driver_vevo.driver.find_element_by_xpath(casino_tarih)
+    cas_tarih.click()
+    cas_tarih.clear()
+    cas_tarih.send_keys(tarih_sutunu[-1])
+    driver_vevo.driver.find_element_by_xpath(casino_bosluk).click()
+    time.sleep(2)
+    driver_vevo.driver.find_element_by_xpath(casino_har_tip).click()
+    time.sleep(2)
+    driver_vevo.driver.find_element_by_xpath(casino_playbet).click()
+    time.sleep(2)
+    driver_vevo.driver.find_element_by_xpath(casino_bahis_ara).click()
+    time.sleep(2)    
+
+    bahis_miktari = driver_vevo.driver.find_element_by_xpath(casino_toplam_bahis).text
+    filtered_bahis_miktari = bahis_miktari.split('.')
+    
+    deposit_miktari -= int(filtered_bahis_miktari[0].replace(',', ''))
+    driver_vevo.driver.find_element_by_xpath(casino_kapat).click()
+    time.sleep(1)
+    kod_yapistir.clear()
+    if deposit_miktari <= 0:
+        a = istatistik()
+        if a != None:
+            note = f'{all_customer_ids[-1]} - Casino cevrimi onay -ILK CEKIM-'
+        else:
+            note = f'{all_customer_ids[-1]} - Casino cevrimi onay'
+        cekim_onay(note)
+    else:
+        note = f'{all_customer_ids[-1]} - Casino cevrimi red'
+        a = istatistik()
+        cekim_red(note)
+
+def cevrim_hesapla(miktar_sutunu, islem_sutunu, tarih_sutunu):
+    x = int(miktar_sutunu[-1])
+    deposit_miktari = x * 7 / 10
+    print(deposit_miktari)
+    if islem_sutunu[-1] in tum_yatirim_yontemleri:
+        for i in range(len(islem_sutunu)-1, 0, -1):
+            if 'Bet' in islem_sutunu[i-1]:
+                bet_id = driver_vevo.driver.find_element_by_xpath(
+                    f'/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[2]/table/tbody/tr[{i}]/td[6]')           
+                bet_id.location_once_scrolled_into_view
+                time.sleep(1)
+                bet_id.click()
+                time.sleep(2)
+                del tarih_sutunu[-1]
+                try:
+                    bet_oranlari = driver_vevo.driver.find_elements_by_xpath(bet)
+                except ElementClickInterceptedException:
+                    time.sleep(2)
+
+                for bet_orani in bet_oranlari:
+                    a = float(bet_orani.text)
+                    if a >= 1.30:
+                        deposit_miktari -= int(miktar_sutunu[i-1])
+                        webdriver.ActionChains(driver_vevo.driver).send_keys(Keys.ESCAPE).perform()
+                        time.sleep(1)
+                        break
+                    else:
+                        webdriver.ActionChains(driver_vevo.driver).send_keys(Keys.ESCAPE).perform()
+                        time.sleep(1)
+                        break
+
+                if deposit_miktari <= 0:
+                    a = istatistik()
+                    if a != None:
+                        note = f'{all_customer_ids[-1]} - Cevrim OK -ILK CEKIM-'
+                    else:
+                        note = f'{all_customer_ids[-1]} - Cevrim OK'
+                    cekim_onay(note)
+                    break
+                if i == 1:
+                    note = f'{all_customer_ids[-1]} - Cevrim RED. Oynamasi gereken tutar: {deposit_miktari}'
+                    cekim_red(note)
+                    break
+            elif i == 1 and len(islem_sutunu) == 1:
+                note = f'{all_customer_ids[-1]} - Cevrim RED. Oynamasi gereken tutar: {deposit_miktari}'
+                cekim_red(note)
+                break
+
+            elif islem_sutunu[i-1] == 'Transfer to Casino':
+                del tarih_sutunu[-1]
+                casino_hesapla(deposit_miktari)
+                break
+
+            elif islem_sutunu[i-1] == 'Transfer To Klas Poker':
+                a = istatistik()
+                if a != None:
+                    note = f'{all_customer_ids[-1]} - Klas Poker -ILK CEKIM-'
+                else:
+                    note = f'{all_customer_ids[-1]} - Klas Poker'
+                cekim_onay(note)
+
+    elif islem_sutunu[-1] not in tum_yatirim_yontemleri:
+        global cr_page_number
+        total_page_nr = driver_vevo.driver.find_elements_by_xpath(
+            '/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[3]/span/a')
+
+        for i in range(cr_page_number, len(total_page_nr)):
+            driver_vevo.driver.find_element_by_xpath(
+                f'/html/body/div[4]/div/div[5]/div/form[2]/fieldset/div/div/div/div[1]/div[9]/div/div/div/div[1]/div[2]/div/div[3]/span/a[{i}]').click()
+            cr_page_number += 1
+            break
+        time.sleep(2)
+        get_wd_data()
+
+check_exists_by_xpath(username_field)
+login()
+
+### 2K USTU CEKIMLERI KT ETME
+### IKINCI VEYA UCUNCU SAYFADA DEP BULURSA ONCEKI SAYFALARDA DA BETE BAKSIN
+### CEKIMI SEC GECERLIYE AT
